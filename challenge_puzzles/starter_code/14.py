395df9a4c2f40113ea38e3eaccda72131032069a
Charles Ikegwu <chaikegwu@gmail.com>
2025-06-23 23:13:36 +0100

    complete puzzle 14

--- challenge_puzzles/starter_code/14.py
+++ challenge_puzzles/starter_code/14.py
@@ -1,5 +1,18 @@
+
+
+
 def reverse_first_five_positions(input_nums: list[int]) -> list[int]:
-    # Your implementation here
+
+    '''
+    Function returns new list with first five elements of original list reversed
+    Make use of python slicing. No loop are to be used.
+    
+    '''
+    first_half = input_nums[4::-1]
+    
+    second_half = input_nums[5:]
+    return first_half + second_half
+    
 
 
 print(reverse_first_five_positions([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
