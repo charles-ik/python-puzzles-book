a0b50ff0d1abe0244b32eb9181a2f59f6f6b6fe8
Charles Ikegwu <chaikegwu@gmail.com>
2025-06-24 10:43:08 +0100

    started 16

--- challenge_puzzles/starter_code/16.py
+++ challenge_puzzles/starter_code/16.py
@@ -1,5 +1,12 @@
 def censor_python(input_strs: list[str]) -> list[str]:
-    # Your implementation here
+    
+    '''
+    When called, the function should return a new list of strings with the letters
+    “P”, “Y”, “T”, “H”, “O”, “N” replaced with “X”, the solution should be case
+    insensitive.
+    '''
+
+    return 
 
 
 print(censor_python(["python", "hello", "HELLO"]))
