f7fbc2a511db3fe66c0b1be3c609b727ac0eab94
Charles Ikegwu <chaikegwu@gmail.com>
2025-06-24 10:41:08 +0100

    completed puzzle 15

--- challenge_puzzles/starter_code/15.py
+++ challenge_puzzles/starter_code/15.py
@@ -1,5 +1,15 @@
 def filter_palindromes(input_strs: list[str]) -> list[str]:
-    # Your implementation here
+    '''
+    Function only returns a new list containing only the strings 
+    which are palindrome (same forwards and backward) like racecar
+    '''
+    output_list = []
+    for element in input_strs:
+        if element[::-1] == element:
+            output_list.append(element)
+    
+    return output_list
+
 
 print(filter_palindromes(["cat", "dog", "racecar", "deified", "giraffe"]))
 print(filter_palindromes(["kayak", "deified", "rotator", "repaper", "deed", "a"]))
