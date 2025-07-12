eccbd47dfa2c74b60745836072eff7ec35bd345e
Charles Ikegwu <chaikegwu@gmail.com>
2025-07-12 18:44:53 +0100

    save

--- challenge_puzzles/starter_code/17.py
+++ challenge_puzzles/starter_code/17.py
@@ -28,6 +28,14 @@ def check_if_string_is_happy(input_str: str) -> bool:
             return False
     return True
 
+## another solution
+def check_if_string_is_happy(input_str: str) -> bool:
+    return not any(
+        a == b or a==c or b == c 
+        for a,b,c in zip(input_str, input_str[1:],input_str[2:]) 
+    )
+
+
 
 print(check_if_string_is_happy("abcdefg"))
 print(check_if_string_is_happy("abcabcabcabc"))
