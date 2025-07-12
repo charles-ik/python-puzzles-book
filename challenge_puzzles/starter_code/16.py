774319fc8bc2ce18283b9e8c486b9f0aaf4c6a59
Charles Ikegwu <chaikegwu@gmail.com>
2025-07-12 18:40:06 +0100

    solution for 16 and 17 added

--- challenge_puzzles/starter_code/16.py
+++ challenge_puzzles/starter_code/16.py
@@ -1,16 +1,22 @@
-def is_censored_char(input_string: str) -> bool:
+def is_censored_char(input_string: str) -> list[str]:
+
 
     censored_chars = "PYTHON"
 
-    for string in input_string:
-        if string in censored_chars:
+    new_list =[]
+    for letter in input_string:
+        if letter.upper() in censored_chars:
+            new_list.append('X')
+            # is_censored = True
+        else:
+            new_list.append(letter)
             
     
     
-    return new_string
+    return new_list
 
 
-print(is_censored_char('PsdXdsYdsthonTHON'))
+# print(is_censored_char('PsdXdsYdsthonTHON'))
 
 
 def censor_python(input_strs: list[str]) -> list[str]:
@@ -20,11 +26,93 @@ def censor_python(input_strs: list[str]) -> list[str]:
     “P”, “Y”, “T”, “H”, “O”, “N” replaced with “X”, the solution should be case
     insensitive.
     '''
-    censored_chars = "PYTHON"
 
-    return 
+    new_list = []
+    for word in input_strs: 
+        lst1 = is_censored_char(word)
+        new_word = ''.join(lst1)
+        new_list.append(new_word)
+
+    return new_list
 
 
-# print(censor_python(["python", "hello", "HELLO"]))
-# print(censor_python(["abcdefg"]))
-# print(censor_python([]))
+
+print(censor_python(["python", "hello", "HELLO"]))
+print(censor_python(["abcdefg"]))
+print(censor_python([]))
+
+
+
+
+# tip for review 
+# you can concatonate strings so something like 
+
+'''
+def censor_python(input_strs: list[str]) -> list[str]:
+    # What letters need to be replaced?
+    censored_letters = "PYTHON"  # Work in uppercase for easier checking
+    
+    # Process each string in the list
+    result = []
+    for string in input_strs:
+        # For each string, build a new censored version
+        new_string = ""
+        for char in string:
+            # Check each character...
+            if char.upper() in censored_letters:
+                new_string += "X"  # What should go here?
+            else:
+                new_string += char  # What should go here?
+        result.append(new_string)
+    
+    return result
+'''
+        
+# other great solutions 
+
+'''
+def censor_python(input_strs: list[str]) -> list[str]:
+    censored = "PYTHON"
+    return [''.join('X' if c.upper() in censored else c for c in s) 
+            for s in input_strs]
+
+
+def censor_python(input_strs: list[str]) -> list[str]:
+    # Create translation table for both upper and lowercase
+    trans_table = str.maketrans('PYTHONpython', 'X' * 12)
+    return [s.translate(trans_table) for s in input_strs]
+
+
+import re
+
+def censor_python(input_strs: list[str]) -> list[str]:
+    # [PpYyTtHhOoNn] matches any of these characters
+    pattern = re.compile(r'[PYTHON]', re.IGNORECASE)
+    return [pattern.sub('X', s) for s in input_strs]
+
+
+def censor_python(input_strs: list[str]) -> list[str]:
+    censored = set('PYTHON')  # Set for O(1) lookup
+    
+    def censor_char(c):
+        return 'X' if c.upper() in censored else c
+    
+    def censor_string(s):
+        return ''.join(map(censor_char, s))
+    
+    return list(map(censor_string, input_strs))
+
+
+def censor_python(input_strs: list[str]) -> list[str]:
+    censored = frozenset('PYTHON')  # Immutable set
+    
+    def censor_string(s):
+        return ''.join('X' if c.upper() in censored else c for c in s)
+    
+    # Generator expression - processes one at a time
+    return list(censor_string(s) for s in input_strs)
+
+'''
+    
+
+
--- challenge_puzzles/starter_code/17.py
+++ challenge_puzzles/starter_code/17.py
@@ -1,5 +1,33 @@
+from collections import defaultdict
+
+#String is happy if the consecutive characters are different
+
+
+
+
+# this works for two consecutive characters 
 def check_if_string_is_happy(input_str: str) -> bool:
-    # Your implementation here
+
+    string_dic = defaultdict(str)
+    
+    for char in input_str: 
+        if char == string_dic['previous string']:
+            is_happy = False
+            break
+        else:
+            is_happy = True
+        string_dic['previous string'] = char
+
+    return is_happy
+
+
+def check_if_string_is_happy(input_str: str) -> bool:
+    for i in range(len(input_str) - 2): #makes sure exactly three characters are checked at the end of string
+        three_chars = input_str[i:i+3] #note: slicing doesn't error if we go over 3 characters. python returns the last character instead. 
+        if len(set(three_chars)) != 3:
+            return False
+    return True
+
 
 print(check_if_string_is_happy("abcdefg"))
 print(check_if_string_is_happy("abcabcabcabc"))
