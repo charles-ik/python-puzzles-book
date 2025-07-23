9cd27256ccec6beeca782202a934097bfec131eb
Charles Ikegwu <chaikegwu@gmail.com>
2025-07-23 07:51:05 +0100

    most up to date on windows laptop

--- challenge_puzzles/starter_code/18.py
+++ challenge_puzzles/starter_code/18.py
@@ -1,5 +1,28 @@
+#What a recursive function 
+# A function that calls itself
+
+#example: 
+
+'''
+def factorial(n):
+    if n == 1:
+        return 1  # base case
+    return n * factorial(n - 1)  # recursive call
+'''
+
+
+
 def get_number_of_digits(input_num: int) -> int:
-    # Your implementation here
+    '''
+    When called, the function should return the number of digits in the input_num.
+        Caveats:
+        The function should be recursive
+        The function should not convert the integer to a string
+    
+    '''
+    
+    count += 1 
+    return count
 
 
 print(get_number_of_digits(1234))
